6f54a6cd2f0d018542602b9e23447d166e424d80
xiedaokun <102251868@qq.com>
2023-07-05 14:40:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-gantt-basic-clzoqz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-gantt-basic-clzoqz)
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <DefaultTheme>
+    <Gantt :tasks="tasks" :links="links" :scales="scales" :columns="columns" />
+  </DefaultTheme>
+</template>
+
+<script>
+  import { Gantt, DefaultTheme } from "@dhtmlx/trial-vue-gantt";
+  import { columns, scales, tasks, links } from "./data";
+
+  export default {
+    name: "App",
+    components: {
+      Gantt, DefaultTheme
+    },
+    data: () => ({
+      columns,
+      scales,
+      tasks,
+      links
+    })
+  };
+</script>
